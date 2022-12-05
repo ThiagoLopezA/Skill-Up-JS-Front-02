ec539d28250dc37e19eb6990a0228ea3dbc3639d
Evange-Fernandez <fernandez.eva.tdf@gmail.com>
2022-12-05 15:05:42 -0300

    <feat>Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 
 const Footer = () => {
+  const socialMedia = [
+    {
+      id: 1,
+      name: "linkedIn",
+      icon: <i class="bi bi-linkedin"></i>,
+      link: "https://www.linkedin.com/",
+    },
+    {
+      id: 2,
+      name: "Instagram",
+      icon: <i class="bi bi-instagram"></i>,
+      link: "https://www.instagram.com/",
+    },
+    {
+      id: 3,
+      name: "Facebook",
+      icon: <i class="bi bi-facebook"></i>,
+      link: "https://www.facebook.com/",
+    },
+  ];
+  return (
+    <div>
+      <footer className="text-black py-4 bg-white">
+        <div className="container">
+          <div>{socialMedia.map(({ id, name, icon, link }) => {
+            <div key={id}>
 
-  return <div>Footer</div>;
+            </div>
+          })}</div>
+          <div className="text-start">
+            © Copyrigth, 2022. All rights reserved
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
