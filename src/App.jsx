1a54251011188e5e4f1eee82bd49dddb0278c581
Evangelina Fernández <99969703+evita-fernan@users.noreply.github.com>
2022-12-05 19:59:48 -0300

    <feat>Footer (#2)
    * <feat>Bootstrap and Bootstrap Icon installed
    
    * <feat>Footer
    
    * Footer with social media links
    
    * <fix>prop in className

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import ExampleContainer from './Components/ExampleComponent/ExampleContainer'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ExampleContainer from "./Components/ExampleComponent/ExampleContainer";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -25,13 +26,13 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <ExampleContainer/>
+      <ExampleContainer />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 
 const Footer = () => {
-
-  return <div>Footer</div>;
+  const socialMedia = [
+    {
+      name: "linkedIn",
+      icon: <i className="bi bi-linkedin"></i>,
+      link: "https://www.linkedin.com/",
+    },
+    {
+      name: "Instagram",
+      icon: <i className="bi bi-instagram"></i>,
+      link: "https://www.instagram.com/",
+    },
+    {
+      name: "Facebook",
+      icon: <i className="bi bi-facebook"></i>,
+      link: "https://www.facebook.com/",
+    },
+  ];
+  return (
+    <div>
+      <footer className="text-black py-4 bg-white">
+        <div className="container align-items-center">
+          <nav className="row">
+            <div className="text-start col">
+              <p className="fs-6"> © Copyright, 2022. All rights reserved</p>
+            </div>
+            <ul className="col list-unstyled d-flex justify-content-end">
+              {socialMedia.map((data) => {
+                return (
+                  <li key={data.name} className="px-4">
+                    <a href={data.link} target="_blank" className="text-reset">
+                      {data.icon}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
