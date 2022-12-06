75766819328ee26cf9c96d412f24d73025d00666
Evangelina Fernández <99969703+evita-fernan@users.noreply.github.com>
2022-12-06 16:48:53 -0300

    <fix>Bootstrap and Bootstrap Icon installed (#4)
    * <feat>Bootstrap and Bootstrap Icon installed
    
    * <feat>Footer
    
    * Footer with social media links
    
    * <fix>prop in className
    
    * |<fix>CDN Bootstrap JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AppRoutes from "./routes";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <Router>
       <AppRoutes />
     </Router>
+    <Footer />
   );
 }
 
