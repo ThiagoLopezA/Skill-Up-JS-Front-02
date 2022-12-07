2c91b9cd41e57b1a694c6fd8242b94d7fda6e589
Thiago López <87744456+ThiagoLopezA@users.noreply.github.com>
2022-12-07 17:17:11 -0300

    <feat>(components) : Header component (#6)
    * <feat>(components): navbar created
    
    * <feat>(components): navbar-container created
    
    * <fix>[components]: navbar acts like a SPA

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AppRoutes from "./routes";
 import Footer from "./Components/Footer/Footer";
+import NavbarContainer from "./Components/Navbar/NavbarContainer";
 
 function App() {
   return (
     <Router>
+      <NavbarContainer />
       <AppRoutes />
+      <Footer />
     </Router>
-    <Footer />
   );
 }
 
