7531434ff0186ad99f45cf902317656ff853ebf1
Irupe <lilianirupemeza@gmail.com>
2022-12-10 17:32:06 -0300

    <fix>[services] using axios config

---
+++ src/services/httpRequest.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+const httpRequest = (method, endpoint, data, headers = {}) => {
+  const config = {
+    headers,
+  };
+
+  const { token } = JSON.parse(localStorage.getItem("token")) || "";
+
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  config.method = method;
+
+  config.url = import.meta.env.URL_API + endpoint;
+  
+  if (data) config.data = data;
+
+  return new Promise((resolve, reject) => {
+    axios(config)
+      .then((res) => resolve(res.data))
+      .catch((error) => reject(error));
+  });
+};
+
+export const getRequest = (endpoint, headers) =>
+  httpRequest("get", endpoint, null, headers);
+
+
+export const postRequest = (endpoint, data, headers) =>
+  httpRequest("post", endpoint, data, headers);
+
