c79d99e94c7e3e3ea9ec6f45b285a08511e254ec
Irupe <lilianirupemeza@gmail.com>
2022-12-10 14:56:53 -0300

    <fix>[services] variable name

--- src/services/service-get.js
+++ src/services/service-get.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import.meta.env.VITE_API
+import.meta.env.URL_API
 
 export function requestGet(path, data) {
     return async function () {
       try {
-        var json = await axios.get(VITE_API + path + data);
+        var json = await axios.get(URL_API + path + data);
         return json.data;
       } catch (error) {
         return error;
--- src/services/service-post.js
+++ src/services/service-post.js
@@ -1,13 +1,13 @@
 
 import axios from "axios";
-import.meta.env.VITE_API
+import.meta.env.URL_API
 //const {token} = JSON.parse(localStorage.getItem('token'))
 
 
 export function requestPost (token, path, data) {
     
         return axios
-          .post(VITE_API + path, data, {
+          .post(URL_API + path, data, {
             headers: { authorization: `Bearer ${token}` },
           })
           .then((response) => response)
