68fa75259f40b78b3fce953d7f2c8989d7d322fb
Pabpol <pabpolval@gmail.com>
2022-12-11 18:20:56 -0300

    feat(router): add edit-user route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,12 +4,14 @@ import Transactions from "../Components/Transactions/Transactions";
 import "../App.css";
 import Home from "../Components/Home";
 import UserRegistrationForm from '../Components/Forms/UserForm/UserRegistrationForm';
+import EditUserForm from '../Components/Forms/UserForm/EditUserForm';
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/transactions" element={<Transactions />} />
     <Route path="/signup" element={<UserRegistrationForm />} />
+    <Route path="/edit-user" element={<EditUserForm user={{ firstName: "Pablo", lastName: 'Polanco', email: "Pablo_Polanco@mail.com" }} />} />
 
   </Routes>
 );
