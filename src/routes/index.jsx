37ffc082c41fc58e8426136bedf0a5617c277e64
Pabpol <pabpolval@gmail.com>
2022-12-11 17:21:20 -0300

    feat(router): add signup route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,11 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Transactions from "../Components/Transactions/Transactions";
 import "../App.css";
 import Home from "../Components/Home";
+import UserRegistrationForm from '../Components/Forms/UserForm/UserRegistrationForm';
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/transactions" element={<Transactions />} />
+    <Route path="/signup" element={<UserRegistrationForm />} />
+
   </Routes>
 );
 
