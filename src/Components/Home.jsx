56a221300cbed6fb2380ca9d39e506e98d1ef95b
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-12 21:28:53 -0300

    feat(pages,components): login page file & login form file created

--- src/Components/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Button from "./Button/Button";
-import TransactionForm from "./Forms/TransactionForm/TransactionForm";
-
-const Home = () => {
-  return <>Home</>;
-};
-
-export default Home;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Transactions from "../Components/Transactions/Transactions";
 import Home from "../Components/Home";
 import ClientLayout from "../Components/ClientLayout/ClientLayout";
+import Login from "../pages/Login";
 
 const AppRoutes = () => (
   <Routes>
     <Route element={<ClientLayout />}>
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Home />} />
       <Route path="/transactions" element={<Transactions />} />
     </Route>
