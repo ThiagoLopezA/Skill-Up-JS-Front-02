9cdf71295fdf8732d55fe6c8ea2d22fa3efeb74b
Thiago López <87744456+ThiagoLopezA@users.noreply.github.com>
2022-12-13 10:46:52 -0300

    feat(pages): Login page (#19)
    * feat(pages,components): login page file & login form file created
    
    * feat(components): login form structure created
    
    * feat(views): login view created

---
+++ src/pages/Login/components/LoginFooter/LoginFooter.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./LoginFooter.module.css";
+export default function LoginFooter() {
+  return (
+    <div className={styles.footer}>
+      <svg
+        width="1em"
+        height="1em"
+        className={styles.icon}
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        font-size="24"
+      >
+        <defs>
+          <path
+            d="M11.987 18.648V11.75H7.221a18.237 18.237 0 01-.522-3.845c2.054-.398 4.24-2.061 5.274-2.933l.014.01v6.768h4.709c-1.176 4.126-3.708 6.212-4.709 6.897M18.157 6.3c-1.89 0-4.691-2.22-5.61-3.074a.84.84 0 00-1.148 0C10.483 4.082 7.696 6.3 5.843 6.3A.844.844 0 005 7.146c0 10.362 6.374 13.174 6.647 13.288a.842.842 0 00.727-.035C12.644 20.253 19 16.707 19 7.146a.844.844 0 00-.843-.846"
+            id="SecurityIcon_41916383649617517_a"
+          ></path>
+        </defs>
+        <use
+          fill="currentColor"
+          xlinkHref="#SecurityIcon_41916383649617517_a"
+          fill-rule="evenodd"
+        ></use>
+      </svg>
+      <div>
+        <h5 className={styles.footerTitle}>Sentite seguro con Alkibank.</h5>
+        <p className={styles.footerText}>
+          Nunca compartas tus datos personales, claves y números de tarjetas con
+          nadie.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Transactions from "../Components/Transactions/Transactions";
-import Home from "../Components/Home";
+import Home from "../pages/Home";
 import ClientLayout from "../Components/ClientLayout/ClientLayout";
+import Login from "../pages/Login/Login";
 
 const AppRoutes = () => (
   <Routes>
+    <Route path="/login" element={<Login />} />
     <Route element={<ClientLayout />}>
       <Route path="/" element={<Home />} />
       <Route path="/transactions" element={<Transactions />} />
