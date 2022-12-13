54c1336e846d0b844e0121b9ede795589855ccb2
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-12 21:34:13 -0300

    feat(components): login form structure created

--- src/Components/Forms/LoginForm/LoginForm.jsx
+++ src/Components/Forms/LoginForm/LoginForm.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import Button from "../../Button/Button";
+import InputField from "../../InputField/InputField";
 
 export default function LoginForm() {
-  return <div>LoginForm</div>;
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const validationSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  });
+  const onSubmit = values => {};
+  const formik = useFormik({ initialValues, validationSchema, onSubmit });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <InputField
+        name="email"
+        type="email"
+        placeholder="Email"
+        formik={formik}
+      />
+      <InputField
+        name="email"
+        type="password"
+        placeholder="Contraseña"
+        formik={formik}
+      />
+      <Button type="submit" variant="primary">
+        Iniciar sesión
+      </Button>
+    </form>
+  );
 }
