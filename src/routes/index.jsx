22402c2d8131fad84c0e959bbd22e06a68789260
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-13 20:21:21 -0300

    feat(pages): register page file created and established in router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-
-import ClientLayout from '../Components/ClientLayout/ClientLayout'
-import Login from '../pages/Login/Login'
-import Home from '../pages/Home'
-import BackofficeContainer from '../pages/Backoffice/BackofficeContainer'
-import Transactions from '../Components/Transactions/Transactions'
-import UserRegistrationForm from '../Components/Forms/UserForm/UserRegistrationForm';
-import EditUserForm from '../Components/Forms/UserForm/EditUserForm';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ClientLayout from "../Components/ClientLayout/ClientLayout";
+import Login from "../pages/Login/Login";
+import Home from "../pages/Home";
+import BackofficeContainer from "../pages/Backoffice/BackofficeContainer";
+import Transactions from "../Components/Transactions/Transactions";
+import UserRegistrationForm from "../Components/Forms/UserForm/UserRegistrationForm";
+import EditUserForm from "../Components/Forms/UserForm/EditUserForm";
+import Register from "../pages/Register/Register";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
     <Route element={<ClientLayout />}>
       <Route path="/" element={<Home />} />
       <Route path="/transactions" element={<Transactions />} />
       <Route path="/signup" element={<UserRegistrationForm />} />
-      <Route path="/edit-user" element={<EditUserForm user={{ firstName: "Pablo", lastName: 'Polanco', email: "Pablo_Polanco@mail.com" }} />} />
+      <Route
+        path="/edit-user"
+        element={
+          <EditUserForm
+            user={{
+              firstName: "Pablo",
+              lastName: "Polanco",
+              email: "Pablo_Polanco@mail.com",
+            }}
+          />
+        }
+      />
       <Route path="/backoffice" element={<BackofficeContainer />} />
     </Route>
   </Routes>
-)
+);
 
-export default AppRoutes
+export default AppRoutes;
