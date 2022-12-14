0d15bd11ddbced9d1d79890e9ea66e00df7932f4
Tomás Salina <salinatomass53@gmail.com>
2022-12-14 03:13:30 -0300

    <feat>(pages): Not Found

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,8 +6,9 @@ import Login from '../pages/Login/Login'
 import Home from '../pages/Home'
 import BackofficeContainer from '../pages/Backoffice/BackofficeContainer'
 import Transactions from '../Components/Transactions/Transactions'
-import UserRegistrationForm from '../Components/Forms/UserForm/UserRegistrationForm';
-import EditUserForm from '../Components/Forms/UserForm/EditUserForm';
+import UserRegistrationForm from '../Components/Forms/UserForm/UserRegistrationForm'
+import EditUserForm from '../Components/Forms/UserForm/EditUserForm'
+import NotFound from '../pages/NotFound'
 
 const AppRoutes = () => (
   <Routes>
@@ -16,8 +17,20 @@ const AppRoutes = () => (
       <Route path="/" element={<Home />} />
       <Route path="/transactions" element={<Transactions />} />
       <Route path="/signup" element={<UserRegistrationForm />} />
-      <Route path="/edit-user" element={<EditUserForm user={{ firstName: "Pablo", lastName: 'Polanco', email: "Pablo_Polanco@mail.com" }} />} />
+      <Route
+        path="/edit-user"
+        element={
+          <EditUserForm
+            user={{
+              firstName: 'Pablo',
+              lastName: 'Polanco',
+              email: 'Pablo_Polanco@mail.com',
+            }}
+          />
+        }
+      />
       <Route path="/backoffice" element={<BackofficeContainer />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   </Routes>
 )
