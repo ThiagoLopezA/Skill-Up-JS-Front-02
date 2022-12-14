f0164a9ac8195069a51c32a6774de1e2260f7cf6
Thiago López <thiagolopezalderete@hotmail.com>
2022-12-14 19:52:31 -0300

    feat(login): login is now working!

--- src/Components/Forms/LoginForm/LoginForm.jsx
+++ src/Components/Forms/LoginForm/LoginForm.jsx
@@ -3,9 +3,16 @@ import * as yup from "yup";
 import { useFormik } from "formik";
 import Button from "../../Button/Button";
 import InputField from "../../InputField/InputField";
+import { postRequest } from "../../../services/httpRequest";
+import useRedirect from "../../../hooks/useRedirect";
 
 export default function LoginForm() {
+  const handleRedirect = useRedirect();
   const required = "Este campo es requerido";
+  const handleErrors = () => {
+    formik.errors.email = "Email o contraseña incorrectos";
+    formik.errors.password = "Email o contraseña incorrectos";
+  };
   const initialValues = {
     email: "",
     password: "",
@@ -14,7 +21,17 @@ export default function LoginForm() {
     email: yup.string().email().required(required),
     password: yup.string().required(required),
   });
-  const onSubmit = values => {};
+  const onSubmit = async values => {
+    try {
+      const response = await postRequest("/auth/login", values);
+      if (response.code === 200) {
+        localStorage.setItem("token", response.body);
+        handleRedirect("/");
+      }
+    } catch (err) {
+      handleErrors();
+    }
+  };
   const formik = useFormik({ initialValues, validationSchema, onSubmit });
   return (
     <form onSubmit={formik.handleSubmit}>
