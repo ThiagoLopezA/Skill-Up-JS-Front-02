e2d7642d9b7589b224dfa46f99fbf941ba0690d9
Thiago López <87744456+ThiagoLopezA@users.noreply.github.com>
2022-12-14 18:02:28 -0300

    style(profile): re-style the profile page (#29)

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,138 +1,147 @@
-import Button from '../Button/Button'
-import InputField from '../InputField/InputField'
-import alerts from '../../services/alerts'
+import Button from "../Button/Button";
+import InputField from "../InputField/InputField";
+import alerts from "../../services/alerts";
+import styles from "./Profile.module.css";
 
-import DefaultAvatar from '../../assets/default-avatar.png'
+import DefaultAvatar from "../../assets/default-avatar.png";
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 const Profile = ({ formik, edit, onEdit }) => {
-  const { values, handleSubmit, setFieldValue, isValid } = formik
+  const { values, handleSubmit, setFieldValue, isValid } = formik;
 
   const avatar =
     values.avatar === null
       ? DefaultAvatar
-      : typeof values.avatar === 'string'
+      : typeof values.avatar === "string"
       ? `${API_URL}/uploads/${values.avatar}`
-      : URL.createObjectURL(values.avatar)
+      : URL.createObjectURL(values.avatar);
 
   const handleAvatar = e => {
-    const file = e.target.files[0]
-    setFieldValue('avatar', file)
-  }
+    const file = e.target.files[0];
+    setFieldValue("avatar", file);
+  };
 
   const handleDelete = async () => {
     try {
       const response = await alerts.questionAlert(
-        '¿Estás seguro que quieres eliminar tu cuenta?',
-        'Una vez confirmado no se podrá revertir'
-      )
-      if (response.isConfirmed) console.log('DELETE PROFILE')
+        "¿Estás seguro que quieres eliminar tu cuenta?",
+        "Una vez confirmado no se podrá revertir"
+      );
+      if (response.isConfirmed) console.log("DELETE PROFILE");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   return (
     <div className="container pt-5">
-      <h1 className="mb-4">My Profile</h1>
+      <div className="container-fluid">
+        <form className="row">
+          <div className="col-3">
+            <div className="d-flex flew-column align-items-center">
+              <div className="mb-3 d-flex flex-column align-items-center">
+                <img
+                  className="rounded-circle"
+                  width={150}
+                  height={150}
+                  style={{ objectFit: "cover" }}
+                  src={avatar}
+                  alt="Avatar"
+                />
+                {edit && (
+                  <>
+                    <label htmlFor="avatar" className={styles.imageLabel}>
+                      Cargar imagen
+                    </label>
+                    <input
+                      type="file"
+                      className={styles.inputFile}
+                      name="avatar"
+                      id="avatar"
+                      onChange={handleAvatar}
+                    />
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div>
+              <div className="mb-3">
+                <label htmlFor="firstName" className="form-label">
+                  Nombre
+                </label>
+                <InputField
+                  type="text"
+                  name="firstName"
+                  placeholder="John"
+                  formik={formik}
+                  readOnly={!edit}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="lastName" className="form-label">
+                  Apellido
+                </label>
+                <InputField
+                  type="text"
+                  name="lastName"
+                  placeholder="Doe"
+                  formik={formik}
+                  readOnly={!edit}
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <InputField
+                  type="text"
+                  name="email"
+                  placeholder="name@example.com"
+                  formik={formik}
+                  readOnly={!edit}
+                />
+              </div>
 
-      <div className="d-flex justify-content-between">
-        <h2>Personal information</h2>
-        <div className="d-flex gap-3">
-          <Button
-            type="button"
-            variant="outlined"
-            size="md"
-            handleClick={onEdit}
-            disabled={edit}
-          >
-            Edit
-          </Button>
-          <Button
-            type="button"
-            variant="outlined"
-            size="md"
-            handleClick={handleDelete}
-          >
-            Delete
-          </Button>
-        </div>
+              <div className="d-flex gap-3">
+                <Button
+                  type="button"
+                  variant="outlined"
+                  size="md"
+                  handleClick={onEdit}
+                  disabled={edit}
+                >
+                  Edit
+                </Button>
+                <Button
+                  type="button"
+                  variant="outlined"
+                  size="md"
+                  handleClick={handleDelete}
+                >
+                  Delete
+                </Button>
+                {edit && (
+                  <Button
+                    type="submit"
+                    variant="primary"
+                    size="md"
+                    handleClick={handleSubmit}
+                    disabled={!isValid}
+                  >
+                    Update
+                  </Button>
+                )}
+              </div>
+              {/* CHANGE PASSWORD */}
+            </div>
+          </div>
+        </form>
       </div>
-
-      <form className="mt-3 py-4">
-        <div className="mb-3">
-          <img
-            className="rounded-circle"
-            width={150}
-            height={150}
-            style={{ objectFit: 'cover' }}
-            src={avatar}
-            alt="Avatar"
-          />
-          {edit && (
-            <input
-              type="file"
-              className="form-control-file ms-3"
-              name="avatar"
-              id="avatar"
-              onChange={handleAvatar}
-            />
-          )}
-        </div>
-        <div className="mb-3">
-          <label htmlFor="firstName" className="form-label">
-            First Name
-          </label>
-          <InputField
-            type="text"
-            name="firstName"
-            placeholder="John"
-            formik={formik}
-            readOnly={!edit}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="lastName" className="form-label">
-            Last Name
-          </label>
-          <InputField
-            type="text"
-            name="lastName"
-            placeholder="Doe"
-            formik={formik}
-            readOnly={!edit}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <InputField
-            type="text"
-            name="email"
-            placeholder="name@example.com"
-            formik={formik}
-            readOnly={!edit}
-          />
-        </div>
-
-        {/* CHANGE PASSWORD */}
-
-        {edit && (
-          <Button
-            type="submit"
-            variant="primary"
-            size="md"
-            handleClick={handleSubmit}
-            disabled={!isValid}
-          >
-            Update
-          </Button>
-        )}
-      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
