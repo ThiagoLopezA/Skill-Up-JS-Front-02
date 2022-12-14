b3583c56fe5469dcada69179541c536b9928acd9
Tomás Salina <salinatomass53@gmail.com>
2022-12-14 12:05:07 -0300

    <feat>navbar: add dashboard section when the user is admin

--- src/Components/ClientLayout/ClientLayout.jsx
+++ src/Components/ClientLayout/ClientLayout.jsx
@@ -19,36 +19,5 @@ export default function ClientLayout() {
         </div>
       </div>
     </div>
-    // <nav className="navbar navbar-expand-lg bg-light">
-    //   <div className="container-fluid">
-    //     <a className="navbar-brand" href="#">
-    //       Alkybank
-    //     </a>
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#navbarText"
-    //       aria-controls="navbarText"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-    //     <div className="collapse navbar-collapse" id="navbarText">
-    //       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-    //         {elements.map((e, i) => {
-    //           return (
-    //             <li key={e + i} className="nav-item">
-    //               <Link className="nav-link" to={e.route}>
-    //                 {e.text}
-    //               </Link>
-    //             </li>
-    //           );
-    //         })}
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
   )
 }
